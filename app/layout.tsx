2bc12540fb194a65173bb5a18804332e1cffc9b7
developersayem <developersayem012@gmail.com>
2025-05-02 02:47:29 +0600

    meta favicon changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
   description:
     "Protect your online privacy with Novix VPN. Secure connections, unlimited bandwidth, and global server access.",
   icons: {
-    icon: "/favicon.ico",
+    icon: "/public/favicon.ico",
   },
 };
 
