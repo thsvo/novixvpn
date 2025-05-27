94a4c79a8d5c237877ed1776a58e3bdc94a8a9e3
developersayem <developersayem012@gmail.com>
2025-05-27 23:17:32 +0600

    padding added pn promo banner

--- components/shared/promo-banner.tsx
+++ components/shared/promo-banner.tsx
@@ -6,34 +6,36 @@ export default function PromoBanner() {
   const format = (n: number) => n.toString().padStart(2, "0");
 
   return (
-    <div className="bg-primary/10 p-6 flex justify-between items-center relative">
-      <div>
-        <h2 className="text-xl font-bold text-[#1A1A1A]">
-          Time To Go Turbo! Save 70% Now
-        </h2>
-        <p className="text-[#5F6368]">
-          Offer Ends In{" "}
-          <span className="text-primary font-semibold px-1">
-            {format(days)}
-          </span>{" "}
-          Days
-          <span className="text-primary font-semibold px-1">
-            {format(hours)}
-          </span>{" "}
-          Hours
-          <span className="text-primary font-semibold px-1">
-            {format(minutes)}
-          </span>{" "}
-          Minutes
-          <span className="text-primary font-semibold px-1">
-            {format(seconds)}
-          </span>{" "}
-          Seconds
-        </p>
+    <div className="bg-primary/10 ">
+      <div className="px-28 py-6 flex justify-between items-center relative">
+        <div>
+          <h2 className="text-xl font-bold text-[#1A1A1A]">
+            Time To Go Turbo! Save 70% Now
+          </h2>
+          <p className="text-[#5F6368]">
+            Offer Ends In{" "}
+            <span className="text-primary font-semibold px-1">
+              {format(days)}
+            </span>{" "}
+            Days
+            <span className="text-primary font-semibold px-1">
+              {format(hours)}
+            </span>{" "}
+            Hours
+            <span className="text-primary font-semibold px-1">
+              {format(minutes)}
+            </span>{" "}
+            Minutes
+            <span className="text-primary font-semibold px-1">
+              {format(seconds)}
+            </span>{" "}
+            Seconds
+          </p>
+        </div>
+        <button className="border border-primary px-6 py-2 rounded hover:bg-primary hover:text-white">
+          Save 70%
+        </button>
       </div>
-      <button className="border border-primary px-6 py-2 rounded hover:bg-primary hover:text-white">
-        Save 70%
-      </button>
     </div>
   );
 }
