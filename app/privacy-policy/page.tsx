2a9a20dba88451f366b4b27bfc41e2036c67d962
mahmudul-developer <mahmudul@solvrz.com>
2025-05-31 22:17:28 +0600

    update link in cookie

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -138,7 +138,11 @@ export default function PrivacyPolicy() {
                   We use cookies and other tracking technologies to enhance your
                   experience on our website. For more detailed information about
                   our use of cookies and how you can manage your preferences,
-                  please visit our 5. Sharing of Information Cookie Policy page.
+                  please visit our 5. Sharing of Information{" "}
+                  <a href="/cookie-policy" className="hover:underline">
+                    Cookie Policy
+                  </a>{" "}
+                  page.
                 </p>
               </li>
               <li>
