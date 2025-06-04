b7d234dfbfb37e99e10e1c809881797a533ca7b3
mahmudul-developer <mahmudul@solvrz.com>
2025-06-04 21:35:58 +0600

    add phone number in pivacy page

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -186,11 +186,13 @@ export default function PrivacyPolicy() {
                   Policy, please feel free to contact us
                 </p>
 
-                <p className="text-gray-400 text-sm">
+                <p className="text-gray-600 text-sm">
                   <strong>Company:</strong> CYBERLOOM IT TECHNOLOGIES
                   <br />
                   <strong>Address:</strong> Plot-33, Main Road, Section-10,
                   Senpara Parbata Mirpur - 1216 Bangladesh (BD)
+                  <br />
+                  <strong>Phone:</strong> +8801974523700
                   <br />
                   <strong>Email:</strong>{" "}
                   <a
